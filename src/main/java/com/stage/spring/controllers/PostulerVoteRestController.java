6247cda0ab2d4835bbb7d19b60d798e2e0aef21d
nadahassen <nada.hassen@esprit.tn>
2023-07-05 21:38:51 +0200

    postuler/vote v1

--- src/main/java/com/stage/spring/controllers/PostulerVoteRestController.java
+++ src/main/java/com/stage/spring/controllers/PostulerVoteRestController.java
@@ -1,9 +1,14 @@
 package com.stage.spring.controllers;
 
+import com.stage.spring.entity.Condidature;
+import com.stage.spring.entity.PostulerVote;
+import com.stage.spring.service.ServiceCondidature;
+import com.stage.spring.service.ServicePostulerVote;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -12,6 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostulerVoteRestController
 {
 
+    @Autowired
+    ServicePostulerVote servicePost;
+
+    @GetMapping("/retrieveAllPost")
+    public List<PostulerVote> retrieveAllPost() {
+        List<PostulerVote> list = servicePost.retrieveAllPost();
+        return list;
+    }
+
+    @PostMapping("/addPost")
+    public PostulerVote addPost(@RequestBody PostulerVote A) {
+        return servicePost.addPost(A);
+    }
+
+    @PostMapping("/updatePost")
+    public PostulerVote updatePost(@RequestBody PostulerVote A) {
+        return servicePost.updatePost(A);
+    }
+
+    @DeleteMapping("/deletePost/{id}")
+    public void deletePost(@PathVariable("id") Long id) {
+        servicePost.deletePost(id);
+    }
+
+    @GetMapping("retrievePostById/{id}")
+    public PostulerVote retrievePostById(@PathVariable Long id) {
+        return servicePost.retrievePostById(id);
+    }
 
 
 
--- src/main/java/com/stage/spring/service/ServicePostulerVote.java
+++ src/main/java/com/stage/spring/service/ServicePostulerVote.java
@@ -1,7 +1,12 @@
 package com.stage.spring.service;
 
+import com.stage.spring.entity.Condidature;
 import com.stage.spring.entity.PostulerVote;
+import com.stage.spring.repository.CondidatureRepo;
+import com.stage.spring.repository.PostulerVoteRepository;
+import com.stage.spring.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,30 +15,34 @@ import java.util.List;
 @Slf4j
 public class ServicePostulerVote implements IServicePostulerVote
 {
-
+    @Autowired
+    PostulerVoteRepository postRepo;
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public List<PostulerVote> retrieveAllPost() {
-        return null;
+        return postRepo.findAll();
     }
 
     @Override
     public PostulerVote addPost(PostulerVote a) {
-        return null;
+        return postRepo.save(a);
     }
 
     @Override
     public void deletePost(Long id) {
-
+        postRepo.deleteById(id);
     }
 
     @Override
     public PostulerVote updatePost(PostulerVote a) {
-        return null;
+        return postRepo.save(a);
     }
 
     @Override
     public PostulerVote retrievePostById(Long id) {
-        return null;
+        PostulerVote a =postRepo.findById(id).get();
+        return a;
     }
 }
